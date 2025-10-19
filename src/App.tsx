aef3d8c4c209b8c403f51709e47943538de4dddb
vyapti1990-dev <vyapti1990@gmail.com>
2025-10-18 21:08:49 -0500

    restaurant code added

---
+++ src/App.tsx
@@ -0,0 +1,27 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+// import login from './component/login'
+
+//  parent  component :  SPA  :  single page  application  
+
+//   login.tsx ,  registration.tsx, dashboard.tsx 
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+     
+      <h1 className='read-the-docs'>Vite + React</h1>
+
+
+
+
+      {/*  create login form  with  usernam and password  field  */}
+   
+    </>
+  )
+}
+
+export default App
